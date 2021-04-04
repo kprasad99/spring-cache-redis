9053f75185e3da15570634943bf129f232fe31b5
Karthik Prasad <adkp.mlr@gmail.com>
2021-04-04 13:39:25 +0530

    Add embedded Redis

--- src/test/java/io/github/kprasad99/redis/SpringCacheRedisApplicationTests.java
+++ src/test/java/io/github/kprasad99/redis/SpringCacheRedisApplicationTests.java
@@ -1,9 +1,11 @@
 package io.github.kprasad99.redis;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {TestRedisConfiguration.class})
 class SpringCacheRedisApplicationTests {
 
 	@Test
